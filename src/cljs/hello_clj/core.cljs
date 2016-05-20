81bf5b89b21dd658fa9709b7a5f155716e080738
kprakobkit <kprakobkit@gmail.com>
2016-05-20 14:36:08 -0700

    Adding more text

--- src/cljs/hello_clj/core.cljs
+++ src/cljs/hello_clj/core.cljs
@@ -4,7 +4,7 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello World! This app was deployed by a bash script."}))
+(defonce app-state (atom {:text "Hello World! This app was deployed by a bash script. Adding some more text."}))
 
 (defn root-component [app owner]
   (reify
