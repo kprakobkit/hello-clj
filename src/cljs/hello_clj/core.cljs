6074202fa46c1faa20f06d1dd6d9c0372ca49518
kprakobkit <kprakobkit@gmail.com>
2016-05-21 15:53:27 -0700

    I think this will work

--- src/cljs/hello_clj/core.cljs
+++ src/cljs/hello_clj/core.cljs
@@ -4,7 +4,7 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello World! This app was deployed by a bash script. Adding some more text. And a little bit more from local, with automatic deploy. Again. Please work."}))
+(defonce app-state (atom {:text "Hello World! This app was deployed by a bash script. Adding some more text. And a little bit more from local, with automatic deploy. Again. Please work. I think it will work."}))
 
 (defn root-component [app owner]
   (reify
