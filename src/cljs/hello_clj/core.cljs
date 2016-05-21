2fa6c35411f416cbbd13c615ce79c7f23b399009
kprakobkit <kprakobkit@gmail.com>
2016-05-21 15:34:53 -0700

    Add test to test automatic deployment

--- src/cljs/hello_clj/core.cljs
+++ src/cljs/hello_clj/core.cljs
@@ -4,7 +4,7 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello World! This app was deployed by a bash script. Adding some more text. And a little bit more from local."}))
+(defonce app-state (atom {:text "Hello World! This app was deployed by a bash script. Adding some more text. And a little bit more from local, with automatic deploy."}))
 
 (defn root-component [app owner]
   (reify
